6cd555fc9d360afd0cb1dab7c074941770fec90d
Andri Ma'ruf Anshori <and_drie69@yahoo.com>
2013-06-24 11:06:58 +0700

    form.php

---
+++ form.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Pradesga Indonesia</title>
+<link rel="stylesheet" type="text/css" href="style.css" />
+<link rel="stylesheet" type="text/css" href="style2.css" />
+<!-- <link rel="stylesheet" type="text/css" href="css/style.css" /> -->
+    <link href="../assets/css/bootstrap.css" rel="stylesheet">
+    <link href="../assets/css/bootstrap-responsive.css" rel="stylesheet">
+<noscript>
+  <link rel="stylesheet" type="text/css" href="css/noscript.css" />
+</noscript>
+
+<style>
+/* html {
+	 background: url('assets/images/service.jpg') no-repeat center top; 
+}
+
+body{
+	opacity:0.7;
+} */
+</style>
+
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.0/jquery.min.js"></script>
+<!--<script type="text/javascript" src="js/jquery.eislideshow.js"></script>
+<script type="text/javascript" src="js/jquery.easing.1.3.js"></script>-->
+        <link rel="stylesheet" type="text/css" href="bg/css/main.css" />
+        <link rel="stylesheet" type="text/css" href="bg/css/style1.css" />
+		<script type="text/javascript" src="bg/js/modernizr.custom.86080.js"></script>
+		
+		<script type="text/javascript" src="assets/js/main.js"></script>
+</head>
+
+<body id="page">
+	<div class="wrapper"> 
+		<div class="container main">
+			<div class="row-fluid">
+				<div class="span6">
+					<h1 class="service-h1">COME AND SAY HELLO, WE DON'T BITE </h1>
+					<div class="map">
+						<iframe width="425" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps/ms?msa=0&amp;msid=203091287264739013475.0004df2b79a8fead6c7e9&amp;ie=UTF8&amp;ll=-7.863305,111.442652&amp;spn=0,0&amp;t=m&amp;output=embed"></iframe><br /><small>Lihat <a href="https://maps.google.com/maps/ms?msa=0&amp;msid=203091287264739013475.0004df2b79a8fead6c7e9&amp;ie=UTF8&amp;ll=-7.863305,111.442652&amp;spn=0,0&amp;t=m&amp;source=embed" style="color:#0000FF;text-align:left">Pradesga Indonesia</a> di peta yang lebih besar</small>
+					</div>
+				</div>
+				<div class="span6">
+					<div class="message">
+						<p class="message-head">
+						Complete the form so we can better understand your needs. If you have questions that require immediate attention, please visit us or contact our phone number:
+						</p>
+						<hr class="hr" />
+						<div class="message-form">
+							<div id="response"></div>
+							<form method="post" id="message-form">
+								<table>
+									<tr><td>
+										<input type="text" name="fullname" id="fullname" placeholder="Full Name"><img src="assets/images/ikon-nama.png" />
+									</td></tr>
+									<tr><td>
+										<input type="text" name="email" id="email" placeholder="Email"><img src="assets/images/ikon-email.png" />
+									</td></tr>
+									<tr><td>
+										<input type="text" name="phonenumber" id="phonenumber" placeholder="Phone Number"><img src="assets/images/ikon-tlp.png" />
+									</td></tr>
+									<tr><td>
+										<textarea name="message" id="message" placeholder="Messages"></textarea>
+									</td></tr>
+									<tr><td>
+										<input type="submit" name="submit" id="form-message" value="Send Now">
+									</td></tr>
+								</table>
+							</form>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
